073d1ba3777362eb8f4731b08d65bd1b78a2513b
javajigagansai <saigagan137@gmail.com>
2026-04-13 13:11:10 +0530

    Add files via upload

---
+++ coding/PYTHON/to_express/a-z (rm).py
@@ -0,0 +1,7 @@
+import random
+def ga():
+    words= "abcdefghijklmnopqrstuvwxyz"
+    a=random.choice(words)
+b=random.choice(words)
+c=random.choice(words)
+print(f'a')
---
+++ coding/PYTHON/to_express/for debug.py
@@ -0,0 +1,8 @@
+import phonenumbers
+from phonenumbers import geocoder
+import time,random
+def spt(traget):
+    print(f"[+] phonetracer v2.1 - OSINT")
+    print(f"[*] target: {target}")
+    print(f"[*")
+    print(f"")
